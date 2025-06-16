d1f682dd8f284c1b2438ac1e318b4bcb1ca186a3
7arzz <7arzz404@gmail.com>
2025-06-16 17:53:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed("#dynamic-text", {
-  strings: ["Student", "Web Developer", "Gen-Z","Visioner"],
+  strings: ["Student", "Web Dev", "Gen-Z","Visioner"],
   typeSpeed: 70,
   backSpeed: 50,
   backDelay: 2500,
